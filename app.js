700634317b87a5e8a0bdec5040cd0556dd85977c
mando <nsa06031@gmail.com>
2019-01-16 11:11:20 +0900

    init: setup template engine environment

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const apiRouter = require('./routes/api.js');
 
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +27,10 @@ app.use('/project/:id', express.static(path.join(__dirname, 'public/project')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRouter);
+
+app.get('/pug', (req, res, next) => {
+  res.render('index', {title: 'pug', message: 'hello mando'});
+});
 
 app.use((req, res) => {
   res.status(404).send('Sorry! NOT FOUND');
