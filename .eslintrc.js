c5000a9f3c21280573188e03a8981bd92289e615
mando <nsa06031@gmail.com>
2019-11-02 20:51:36 +0900

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "airbnb-base",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-param-reassign": ["error", { "props": false }],
+        "no-underscore-dangle": "off",
+        "no-unused-expressions": ["warn", { "allowShortCircuit": true }],
+        "max-len": ["error", { "code": 200 }],
+        "no-restricted-globals": "warn",
+        "consistent-return": "warn",
+        "no-unused-vars": "warn",
+    }
+};
